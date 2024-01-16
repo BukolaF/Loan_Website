19a8add594c522ffc9812e246067e29b49b50483
BukolaF <bukolafavour25@gmail.com>
2024-01-16 18:11:15 +0100

    dev-branch

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai';
+import {GiTakeMyMoney} from 'react-icons/gi';
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -10,7 +11,7 @@ const Navbar = () => {
 
   return (
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-blue-800'>
-        <h1 className='w-full text-3xl font-bold text-blue-700'>LOAN WEBSITE</h1>
+        <h1 className=' flex gap-2.5 w-full text-3xl font-bold text-blue-700'>SecuredLoans <GiTakeMyMoney className='h-8 w-8' size={20} /> </h1>
         <ul className='hidden md:flex'>
           <li className='p-8'>loans</li>
           <li className='p-8'>About</li> 
@@ -22,7 +23,7 @@ const Navbar = () => {
           
           </div>  
           <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-blue-200 bg-blue-300 ease-in-out duration-500' : 'fixed left-[-100%]'}>
-          <h1 className='w-full text-3xl font-bold text-blue-700 m-4 pt-6'>LOAN WEBSITE</h1>
+          <h1 className='flex gap-2.5 w-full text-3xl font-bold text-blue-700 m-4 pt-6'>SecuredLoans <GiTakeMyMoney className='h-8 w-8' size={20} /> </h1>
           <ul className=' pt-10 uppercase'>
             <li className='p-8 border-b border-blue-900'>loans</li>
             <li className='p-8 border-b border-blue-900'>About</li>
