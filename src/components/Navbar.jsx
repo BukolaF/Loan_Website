deb8bf90668728211f671531e52f66d1c14575e0
BukolaF <bukolafavour25@gmail.com>
2024-01-16 04:04:17 +0100

    dev-branch

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import React, {useState} from 'react';
+import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai';
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav)
+  }
+
+  return (
+    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-blue-800'>
+        <h1 className='w-full text-3xl font-bold text-blue-700'>LOAN WEBSITE</h1>
+        <ul className='hidden md:flex'>
+          <li className='p-8'>loans</li>
+          <li className='p-8'>About</li> 
+          <li className='p-8'>Login</li>
+          <li className='p-8'>Contact</li>
+        </ul> 
+        <div onClick={handleNav} className='block md:hidden'>
+          {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu  size={20}/>}
+          
+          </div>  
+          <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-blue-200 bg-blue-300 ease-in-out duration-500' : 'fixed left-[-100%]'}>
+          <h1 className='w-full text-3xl font-bold text-blue-700 m-4 pt-6'>LOAN WEBSITE</h1>
+          <ul className=' pt-10 uppercase'>
+            <li className='p-8 border-b border-blue-900'>loans</li>
+            <li className='p-8 border-b border-blue-900'>About</li>
+            <li className='p-8 border-b border-blue-900'>Login</li>
+            <li className='p-8 border-b border-blue-900'>Contact</li>
+        </ul> 
+          </div>
+        </div>
+  )
+}
+
+export default Navbar
+
+
