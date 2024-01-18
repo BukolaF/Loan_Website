52af00d9a9de98f5970e330c585ea51fd3474c8a
BukolaF <bukolafavour25@gmail.com>
2024-01-18 01:49:21 +0100

    home-page-look

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
 
   return (
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 py-4 text-blue-800'>
-        <h1 className=' flex gap-2.5 w-full text-3xl font-bold text-blue-700'>Secured<span className='text-blue-500'>Loans</span> <GiTakeMyMoney className='h-8 w-8' size={20}/></h1>
+        <h1 className=' flex w-full text-3xl font-bold text-blue-700'>Secured<span className='text-blue-500'>Loans</span> <GiTakeMyMoney className='h-8 w-8' size={20}/></h1>
         <ul className='hidden md:flex'>
         {navItems.map(({path,link}) => <li className='p-8'>
         <NavLink to={path}>{link}</NavLink> </li>)}
@@ -31,7 +31,7 @@ const Navbar = () => {
           
           </button>  
           <div className={!showNav ? 'fixed left-0 top-0 w-[70%] h-full border-r border-r-blue-200 bg-blue-300 ease-in-out duration-500' : 'fixed left-[-100%]'}>
-          <h1 className='flex gap-2.5 w-full text-3xl font-bold text-blue-700 m-4 pt-6'>SecuredLoans <GiTakeMyMoney className='h-8 w-8' size={20} />     
+          <h1 className='flex w-full text-3xl font-bold text-blue-700 m-4 pt-6'>Secured<span className='text-blue-500'>Loans</span> <GiTakeMyMoney className='h-8 w-8' size={20} />     
           <IoMdCloseCircle onClick={()=>{handleNav(showNav)}} className='flex justify-center' />
                 </h1>
           <ul className=' pt-10 uppercase'>
