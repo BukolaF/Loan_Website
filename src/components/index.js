254d8dfcffe8e0adbc4e014b5c381703dfacd913
BukolaF <bukolafavour25@gmail.com>
2024-01-22 12:44:58 +0100

    index-file-changes

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,5 @@
 
 
-import { Footer } from './Footer';
-import { Navbar } from './Navbar';
-import { NavItems } from './NavItems';
-import { Hero } from './Hero';
-
-
 export * from './Footer';
 export * from './NavItems';
 export * from './Navbar';
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,3 @@
-import { About } from './About';
-import { Contact } from './Contact';
-import { Home } from './Home';
-import { Loans } from './Loans';
-import { Login } from './Login';
 
 
 export * from './About'
@@ -11,15 +6,4 @@ export * from './Home'
 export * from './Loans'
 export * from './Login'
 
-export const index = () => {
-  return (
-    <div>
-        <About />
-        <Contact />
-        <Home />
-        <Loans />
-        <Login />
-    </div>
-  )
-}
 
